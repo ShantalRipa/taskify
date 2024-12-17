a5cde3b655f67802131a759da29badd2fe877ffd
Valerie Shantal Mac Comish Ripa <valeriemaccomish@Valeries-MacBook-Pro.local>
2024-12-16 18:33:07 -0800

    Add TaskRepository interface for CRUD operations

---
+++ src/main/java/com/valerie/taskify/repository/TaskRepository.java
@@ -0,0 +1,9 @@
+package com.valerie.taskify.repository;
+
+import com.valerie.taskify.entity.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> {
+}
